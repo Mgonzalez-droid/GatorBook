ad6be2acd706dfcfa6031d26a3345d41e6ed2d1f
Alexander Tran <ktran945@gmail.com>
2022-11-02 20:14:02 -0700

    created groups page, table in SQL and JS actions to insert names into table

--- database.js
+++ database.js
@@ -7,14 +7,42 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    insecureAuth : true
+    insecureAuth : true,
+    database: "gatorbookdb"
   });
 
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 
-  
+    //group database actions
+
+
+
+    //var useGDB = "USING gatorbookdb"
+    //var createTABLe = "CREATE table "
+
+  var sql = "INSERT INTO Members (FirstName, LastName) VALUES?"
+
+  var values = [
+    ['John', 'Martin'],
+    ['Thomas','Johnson'],
+    ['James', 'Brown']
+
+  ];
+
+  con.query(sql, [values], function(err, result) {
+
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+
+  });
+
+
+
+
+
+
 
 
     //initialize database, work in progress...
--- database.sql
+++ database.sql
@@ -9,3 +9,14 @@ CREATE TABLE gatorbookdb.user(
 INSERT INTO gatorbookdb.user(username, password) values ("root", "password");
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 flush privileges;
+
+/*Gatorbook groups */
+
+
+CREATE TABLE Members (
+	Memberid int NOT NULL AUTO_INCREMENT,
+	FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+	PRIMARY KEY (Memberid)
+);
+
