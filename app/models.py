a341fddeeb670b5b8eae454d17cb90fffeae7c1e
Alexander Tran <ktran945@gmail.com>
2022-11-24 11:51:04 -0800

    created notifications model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 #models for Message, User and Notifications
 from datetime import datetime 
 from app import db
+import json
+from time import time
 
 class Message(db.Model): 
     id = db.Column(db.Integer, primary_key = True)
@@ -19,3 +21,13 @@ class Post (db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
