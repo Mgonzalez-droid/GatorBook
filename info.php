7aaea78efec85356a513a26595edb9a15e7d9f5e
Ethan <ethanotran@gmail.com>
2022-11-30 23:44:57 -0800

    added changes to info.php and create_user.html based on code analysis

--- info.php
+++ info.php
@@ -6,7 +6,8 @@ $host = "localhost";
 $dbname = "gatorbookdb";
 $username = "root";
 $password = "password";
-function test_input($data) {
+function test_input($data)
+{
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
@@ -14,33 +15,30 @@ function test_input($data) {
 }
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
-if (mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
   die("Connection Error: " . mysqli_connect_error());
 }
 $sample_username = "johndoe";
 $sql_1 = "SELECT * FROM user WHERE username = '$sample_username'";
 $result = mysqli_query($conn, $sql_1);
-if(mysqli_num_rows($result) > 0){
+if (mysqli_num_rows($result) > 0) {
 
 $sql = "INSERT INTO public_post (body)
         VALUES (?)";
 
 $stmt = mysqli_stmt_init($conn);
 
-if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+if (! mysqli_stmt_prepare($stmt, $sql)) {
 
         die(mysqli_error($conn));
     }
 
 
-mysqli_stmt_bind_param($stmt, "s",
-                       $text);
+mysqli_stmt_bind_param($stmt, "s", $text);
 
 mysqli_stmt_execute($stmt);
 
 echo "Post saved.";
-} else{
+} else {
   echo "your username was not found, you are not a user of gatorbook";
 }
-
- ?>
